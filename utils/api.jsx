b463b65ebac248fe6a79469dd2eb696c54aac740
berri-puff <jennyzhang0207@gmail.com>
2024-07-10 15:07:39 +0100

    able to get random objects and pagination of it

--- utils/api.jsx
+++ utils/api.jsx
@@ -4,7 +4,7 @@ const app = axios.create({
     baseURL: "https://api.vam.ac.uk/v2"
 })
 export const getsUKArtworks = () =>{
-   return app.get("/museumobject/O828146").then(({data})=>{
-    return data.record
+   return app.get("/objects/search?random=1&page_size=10&page=1").then(({data})=>{
+    return data.records
    })
 }
