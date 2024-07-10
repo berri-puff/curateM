95729da161161f7a974b6f6822338ddc8a013b9a
berri-puff <jennyzhang0207@gmail.com>
2024-07-10 15:54:17 +0100

    removes the unneeded notes on accessing the for each item

--- src/components/uk-region/uk-main-page.jsx
+++ src/components/uk-region/uk-main-page.jsx
@@ -13,37 +13,18 @@ const UKRegion = () => {
     return (
         <article>
            <h1>this page will contain all the uk artworks only, calling on the V&A api so far, to add Fitzwilliam later on</h1>
-           <p>Notes: Essential information from the results:</p>
-           <ul>
-            <li>accessionNumber</li>
-            <li>ArtistMakerPerson, [0], - association = id, text, -name = id, text</li>
-            <li>briefDescription</li>
-            <li>Categories</li>
-            <li>collectionCode?</li>
-            <li>GalleryLocations array</li>
-            <li>images</li>
-            <li>physicalDescription?</li>
-            <li>placeOfOrigin array</li>
-            <li>productionDates?</li>
-            <li>recordcreation</li>
-            <li>summaryDescription</li>
-            <li>systemNumber</li>
-            <li>techniques array </li>
-            <li>titles array, [0], title, type</li>
-
-           </ul>
+            <h2>When clicking on the image, takes to the single arts page, css when hovered over, display some of the core infromation</h2>
            {ukArtworks.map((artwork)=>{
                return <ol key={artwork.systemNumber}>
-
-                <li >{artwork.objectType}</li>
-                <img src={artwork["_images"][  "_primary_thumbnail"]}></img>
-                <p>{artwork["_primaryMaker"]["name"]}</p>
-                <p>{artwork["systemNumber"]}</p>
-                <p>{artwork["_primaryDate"]}</p>
                 <p>{artwork["_primaryTitle"]}</p>
+                <img src={artwork["_images"][  "_primary_thumbnail"]}></img>
+                <li >Type: {artwork.objectType}</li>
+                <p>By: {artwork["_primaryMaker"]["name"]}</p>
+                {/* <p>{artwork["_primaryDate"]}</p> */}
                 <p>{artwork["_currentLocation"].displayName}</p>
                 </ol>
            })}
+          
         </article>
     
     )
