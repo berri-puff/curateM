31c71957015615bcd3982c405b404a3d92f67db8
berri-puff <jennyzhang0207@gmail.com>
2024-07-10 14:37:12 +0100

    adds some notes on how to access each essential data

--- src/components/uk-region/uk-main-page.jsx
+++ src/components/uk-region/uk-main-page.jsx
@@ -14,6 +14,25 @@ const UKRegion = () => {
     return (
         <article>
            <h1>this page will contain all the uk artworks only, calling on the V&A api so far, to add Fitzwilliam later on</h1>
+           <p>Notes: Essential information from the results:</p>
+           <ul>
+            <li>accessionNumber</li>
+            <li>ArtistMakerPerson, [0], - association = id, text, -name = id, text</li>
+            <li>briefDescription</li>
+            <li>Categories</li>
+            <li>collectionCode?</li>
+            <li>GalleryLocations array</li>
+            <li>images</li>
+            <li>physicalDescription?</li>
+            <li>placeOfOrigin array</li>
+            <li>productionDates?</li>
+            <li>recordcreation</li>
+            <li>summaryDescription</li>
+            <li>systemNumber</li>
+            <li>techniques array </li>
+            <li>titles array, [0], title, type</li>
+
+           </ul>
         </article>
     
     )
