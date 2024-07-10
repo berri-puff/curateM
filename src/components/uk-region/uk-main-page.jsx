fcfcb26bc6ee3cc8fcae155b95d7600116f555f0
berri-puff <jennyzhang0207@gmail.com>
2024-07-10 14:08:41 +0100

    sorts the json object access to the info

--- src/components/uk-region/uk-main-page.jsx
+++ src/components/uk-region/uk-main-page.jsx
@@ -7,14 +7,13 @@ const UKRegion = () => {
     useEffect(()=>{
         getsUKArtworks().then((results)=>{
             setUkArtworks(results)
-            console.log(ukArtworks["summaryDescription"], 'in the page container')
+          
         })
     }, [])
-
+  console.log(ukArtworks, 'in the page container')
     return (
         <article>
            <h1>this page will contain all the uk artworks only, calling on the V&A api so far, to add Fitzwilliam later on</h1>
-        <p>{ukArtworks.summaryDescription}</p> 
         </article>
     
     )
