cb8558e511f4647580a92a5aa951cd587d9395ce
berri-puff <jennyzhang0207@gmail.com>
2024-07-10 12:44:35 +0100

    installs react icons and adds some palceholders for home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react"
-
+import { Link } from "react-router-dom"
+import { FaArrowRight } from "react-icons/fa";
 const Home = ()=>{
 
     return <main>
-        intro on this page -how it works, how to navigate 
-        explore by region and insert links to each of the areas available right now to explore
-        some art work scrolling by
+        <article>
+            Want to start curating your own exhibition artworks but do not know where to start? 
+            On Curate'M, short for Curate Them, right here you can explore art works from different regions, see any you like? Go ahead and favourite them, even add them to your temporary exhibiton file!
+            Currently, Only UK and Us regions are open for business. Get start right now!
+        </article>
+        <h2>EXPLORE BY REGION</h2>
+        <Link to='/uk'>UK Art works <FaArrowRight/> </Link>
+        <Link to='/us'>USA Art works <FaArrowRight/></Link>
+        <h2>Add some artworks auto scrolling down?</h2>
     </main>
 }
 
