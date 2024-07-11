491a2f4426b8966e2ab48544eebfc5404cba1da6
berri-puff <jennyzhang0207@gmail.com>
2024-07-11 16:21:58 +0100

    adds aloadingstate to the single page as well as rendering the correct titles if no title named untitlted

--- src/components/uk-region/uk-single-art.jsx
+++ src/components/uk-region/uk-single-art.jsx
@@ -6,23 +6,35 @@ const UKSingleArt = () =>{
     const [ukSingleArtwork, setUkSingleArtwork] = useState([])
     const {faves, setFaves} = useContext(FavouriteContext)
     const [artCategory, setArtCategory] = useState([])
+    const [loadingState, setLoadingState] = useState([false])
     //use params here to get the artwork id 
 
 const {artId} = useParams()
 
 useEffect(() =>{
+    setLoadingState(true)
     getsUkArtworkById(artId).then((result)=>{
 setUkSingleArtwork(result)
-console.log(ukSingleArtwork)
+setLoadingState(false)
     }).catch((error) =>{
 
     })
 }, [])
+console.log(ukSingleArtwork)
 
-    return <h1>This will contain a single piece of art once user clicks on it/interacts
+if (loadingState) {
+    return <h1>Loading...</h1>
+}else return ( <article>
+        <h1>This will contain a single piece of art once user clicks on it/interacts
         may also have some suggested art via artist names 
         {faves}
+      
     </h1>
+
+{ukSingleArtwork.titles.length != 0? <h2>{ukSingleArtwork.titles[0].title} </h2>: <h2>Untitled Artwork</h2>}
+    </article>
+    )
+    
 }
 
 export default UKSingleArt
