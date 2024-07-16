822558ade72b1d5df267504533dd8ed7276bc5d4
berri-puff <jennyzhang0207@gmail.com>
2024-07-16 16:32:27 +0100

    adds exhibit context to page

--- src/components/us-region/us-single-art.jsx
+++ src/components/us-region/us-single-art.jsx
@@ -2,10 +2,12 @@ import { useState, useContext, useEffect } from "react"
 import { FavouriteContext } from "../../context/faves-context"
 import { useParams } from "react-router-dom"
 import { getSingleUsWork } from "../../../utils/api"
+import { ExhibitContext } from "../../context/exhibit-context"
 
 const USSingleArt = () =>{
     const [usSingleArtwork, setUsSingleArtwork] = useState([])
     const {faves, setFaves} = useContext(FavouriteContext)
+    const {exhibit, setExhibit} = useContext(ExhibitContext)
  const {artId} = useParams()
  console.log(artId, 'in the us singel')
 useEffect(()=>{
