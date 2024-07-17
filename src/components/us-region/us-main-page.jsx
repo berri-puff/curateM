e0156fe9c7d44a336047624505132fbf94e2f05e
berri-puff <jennyzhang0207@gmail.com>
2024-07-17 10:33:14 +0100

    adds an error message on loading main page

--- src/components/us-region/us-main-page.jsx
+++ src/components/us-region/us-main-page.jsx
@@ -7,10 +7,18 @@ const USRegion = () =>{
     const [artCategory, setArtCategory] = useState([])
     const [minimum, setMinimum] = useState(0)
     const [maximum, setMaximum] = useState(19)
+    const [error, setError] = useState([null])
+    const [loading, setLoading] = useState(false)
   
     useEffect(()=>{
+        setLoading(true)
         getUsArtworks().then((results)=>{
             setUsArtworks(results.data)
+            setLoading(false)
+        }).catch((err) =>{
+            console.log(err)
+            setLoading(false)
+            setError(err)
         })
     }, [])
 
@@ -26,6 +34,14 @@ const handlepreviousBatchBtn = () =>{
     setMinimum((currentMin)=> currentMin -= 10)
     setMaximum((currentMax)=> currentMax -= 10)
 }
+
+if (loading) {
+    return <h1>Currently Loading</h1>
+}
+else if (error && usArtworks.length === 0) {
+return <h1>opps, something went wrong, {error.message}</h1>
+}
+else {
 
 
 return (
@@ -53,6 +69,7 @@ return (
             <button aria-label="Next batch" onClick={()=>{handleNextBatchBtn()}}>Next</button>      
     </article>
    )
+}
 }
 
 export default USRegion
