ffa48b1bbd760340434662c79264055f07264e95
berri-puff <jennyzhang0207@gmail.com>
2024-07-20 11:17:12 +0100

    links cleveland museum contact and completes the us art display

--- src/components/exhibits.jsx
+++ src/components/exhibits.jsx
@@ -24,7 +24,7 @@ const Exhibits = () =>{
                    <p>© Victoria and Albert Museum, London</p>
                    <p>ID: {artwork.systemNumber}</p>
                    <p>Found in V&A Museum Collection, on shelf {artwork.galleryLocations[0].shelf}, box {artwork.galleryLocations[0].box}, in case {artwork.galleryLocations[0].case} </p>
-                   <p>Any further questions, feel free to contact Victoria and Albert Museum <a target="blank" href="https://www.vam.ac.uk/info/contact-us">here</a></p>
+                   <p>Contact  <a target="blank" href="https://www.vam.ac.uk/info/contact-us">Victoria and Albert Museum</a> regarding their pieces</p>
                      </article>)
             }
             else {
@@ -32,6 +32,10 @@ const Exhibits = () =>{
                 return (
                     <article>
                          <img src={artwork.images.web.url} alt="No description given"></img>
+                         <p>The Cleveland Museum of Art Open Access API</p>
+                         <p>ID: {artwork.id}</p>
+                         <p>Found in Cleveland Museum of Art, {artwork.department}, {artwork.currentLocation}</p>
+                         <p>Contact <a target="blank" href="https://www.clevelandart.org/contact-us">Cleveland Museum of Art </a> regarding their pieces</p>
                     </article>
                 )
             }
