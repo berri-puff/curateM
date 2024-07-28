98aa90d988ed3533206948ed10c70c05de7d5512
berri-puff <jennyzhang0207@gmail.com>
2024-07-28 09:58:03 +0100

    semi decoration of the nav bar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,12 +1,27 @@
-import { Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import { PiPaintBrushHouseholdFill } from "react-icons/pi";
 
-const Navigation = () =>{
-    return <nav>
-        <Link to="/uk">UK Region</Link>
-        <Link to="/us">USA Region</Link>
-        <Link to="/exhibits">Exhibits</Link>
-        <input type="text" />
+
+const Navigation = () => {
+  return (
+    <nav className="navbar" aria-label="menu" role="navigation">
+      <div className="navbar-item has-dropdown is-hoverable">
+        <a className="navbar-link">Navigation <PiPaintBrushHouseholdFill />
+        </a>
+        <div className="navbar-dropdown">
+          <Link to="/uk" aria-hidden="true" className="navbar-item">
+            UK Region
+          </Link>
+          <Link to="/us" aria-hidden="true" className="navbar-item">
+            USA Region
+          </Link>
+          <Link to="/exhibits" aria-hidden="true" className="navbar-item">
+            Exhibits
+          </Link>
+        </div>
+      </div>
     </nav>
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
