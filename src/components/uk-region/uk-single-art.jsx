fe4d1417a60ff6a2d319431cbd11ba9917ea0351
berri-puff <jennyzhang0207@gmail.com>
2024-07-30 17:38:08 +0100

    makes the where to find as a dropdown menu

--- src/components/uk-region/uk-single-art.jsx
+++ src/components/uk-region/uk-single-art.jsx
@@ -3,6 +3,8 @@ import Error from "../error-page";
 import { useParams } from "react-router-dom";
 import { ExhibitContext } from "../../context/exhibit-context";
 import { getsUkArtworkById } from "../../../utils/api";
+import { IoMdArrowDropdown } from "react-icons/io";
+
 const UKSingleArt = () => {
   const [ukSingleArtwork, setUkSingleArtwork] = useState([]);
   const { setExhibit } = useContext(ExhibitContext);
@@ -50,97 +52,112 @@ const UKSingleArt = () => {
   };
 
   if (loadingState) {
-    return       <progress className="progress is-small is-primary" max="100">20%</progress>
+    return (
+      <progress className="progress is-small is-primary" max="100">
+        20%
+      </progress>
+    );
   } else if (ukSingleArtwork.length === 0 && error) {
     const msg = error.data && error.data.detail;
     return <Error status={error.status} msg={msg} />;
   } else
     return (
       <article>
-   
-          {ukSingleArtwork.titles.length != 0 ? (
+        {ukSingleArtwork.titles.length != 0 ? (
           <h1 className="title is-3">{ukSingleArtwork.titles[0].title} </h1>
         ) : (
           <h1 className="title is-3">Untitled Artwork</h1>
         )}
-     
+
         <p>{feedbackMsg != "" ? <p>{feedbackMsg}</p> : null}</p>
         <p>{exhibitLoad ? <p>adding to your exhibit</p> : null}</p>
-       
-       <section className="columns">
-        <section className="column">
-  {ukSingleArtwork.images.length != 0 ? (
-          <img 
-            src={`https://framemark.vam.ac.uk/collections/${ukSingleArtwork.images[0]}/full/600,400/0/default.jpg`}
-          ></img>
-        ) : (
-          <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/660px-No-Image-Placeholder.svg.png?20200912122019" />
-        )}
-        <p className="">© Victoria and Albert Museum, London</p> 
-          <button
-        className="button is-normal is-rounded is-dark is-primary"
-          aria-label="add to exhibit"
-          onClick={() => {
-            addToExhibit();
-          }}
-          disabled={addBtnDisable}
-        >
-          Add To My Exhibit
-        </button>
-        </section>
-       <section className="column">
- <p>
-          Artist:
-          {ukSingleArtwork.artistMakerPerson.length != 0 ? (
-            <span> {ukSingleArtwork.artistMakerPerson[0].name["text"]}</span>
-          ) : (
-            <span> Unknown</span>
-          )}
-        </p>
-        <h3>About: </h3>
-        {ukSingleArtwork.summaryDescription ? (
-          <p>{ukSingleArtwork.summaryDescription}</p>
-        ) : (
-          <p>{ukSingleArtwork.briefDescription}</p>
-        )}
-        <p>
-          This piece was added to V&A collection in the year 
-          <span>{ukSingleArtwork.accessionYear}</span>
-        </p>
-        {ukSingleArtwork.objectHistory != "" ? (
-          <p>{ukSingleArtwork.objectHistory}</p>
-        ) : null}
-        Made in
-        {ukSingleArtwork.placesOfOrigin.length != 0 ? (
-          <span>{ukSingleArtwork.placesOfOrigin[0].place["text"]}</span>
-        ) : (
-          <span>Unknown</span>
-        )}
-        <p>
-        
-          This piece of work is estimated to be made in 
-          {ukSingleArtwork.productionDates.length != 0 ? (
-            <span>{ukSingleArtwork.productionDates[0].date.text}</span>
-          ) : null}
-        </p>
-        <section className="column">
- <h3>Where to find it</h3>
-        <p>It is currently kept in Victoria and Albert Museum</p>
-        <ul>
-          <li>BOX: {ukSingleArtwork.galleryLocations[0].box}</li>
-          <li>CASE: {ukSingleArtwork.galleryLocations[0].case}</li>
-          <li>SHELF: {ukSingleArtwork.galleryLocations[0].shelf}</li>
-        </ul>
-        </section>
-  
-       </section>
-    
-   </section>
-        
 
-       
-     
-      
+        <section className="columns">
+          <section className="column">
+            {ukSingleArtwork.images.length != 0 ? (
+              <img
+                src={`https://framemark.vam.ac.uk/collections/${ukSingleArtwork.images[0]}/full/600,400/0/default.jpg`}
+              ></img>
+            ) : (
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/660px-No-Image-Placeholder.svg.png?20200912122019" />
+            )}
+            <p className="subtitle is-7">
+              © Victoria and Albert Museum, London
+            </p>
+            <button
+              className="button is-normal is-rounded is-dark is-primary"
+              aria-label="add to exhibit"
+              onClick={() => {
+                addToExhibit();
+              }}
+              disabled={addBtnDisable}
+            >
+              Add To My Exhibit
+            </button>
+          </section>
+          <section className="column">
+            <p>
+              Artist:
+              {ukSingleArtwork.artistMakerPerson.length != 0 ? (
+                <span>
+                  {" "}
+                  {ukSingleArtwork.artistMakerPerson[0].name["text"]}
+                </span>
+              ) : (
+                <span> Unknown</span>
+              )}
+            </p>
+            <h3>About: </h3>
+            {ukSingleArtwork.summaryDescription ? (
+              <p>{ukSingleArtwork.summaryDescription}</p>
+            ) : (
+              <p> {ukSingleArtwork.briefDescription}</p>
+            )}
+            <p>
+              This piece was added to V&A collection in the year
+              <span> {ukSingleArtwork.accessionYear}</span>
+            </p>
+            {ukSingleArtwork.objectHistory != "" ? (
+              <p> {ukSingleArtwork.objectHistory}</p>
+            ) : <p>Little to no history can be found for this artwork.</p>}
+            Made in
+            {ukSingleArtwork.placesOfOrigin.length != 0 ? (
+              <span> {ukSingleArtwork.placesOfOrigin[0].place["text"]}</span>
+            ) : (
+              <span>Unknown</span>
+            )}
+            <p>
+              This piece of work is estimated to be made in
+              {ukSingleArtwork.productionDates.length != 0 ? (
+                <span> {ukSingleArtwork.productionDates[0].date.text}</span>
+              ) : null}
+            </p>
+            <section className="column">
+              <div class="dropdown is-hoverable">
+                <div class="dropdown-trigger">
+                  <button
+                    class="button"
+                    aria-haspopup="true"
+                    aria-controls="dropdown-menu4"
+                  >
+                     <h3>Where to find it <IoMdArrowDropdown />
+                     </h3>
+                   
+                  </button>
+                </div>
+                <div class="dropdown-menu" id="dropdown-menu4" role="menu">
+                  <div class="dropdown-content">
+                  <p className="dropdown-item">BOX: {ukSingleArtwork.galleryLocations[0].box}</p>
+                <p className="dropdown-item">CASE: {ukSingleArtwork.galleryLocations[0].case}</p>
+                <p  className="dropdown-item">SHELF: {ukSingleArtwork.galleryLocations[0].shelf}</p>
+                     
+                    </div>
+                  </div>
+                </div>
+           
+            </section>
+          </section>
+        </section>
       </article>
     );
 };
