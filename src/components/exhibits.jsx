0ebd2224e88d9e28a1fea4c3eb1bb5a902f9058d
berri-puff <jennyzhang0207@gmail.com>
2024-07-31 15:54:24 +0100

    formating some of exhibit

--- src/components/exhibits.jsx
+++ src/components/exhibits.jsx
@@ -3,7 +3,7 @@ import { ExhibitContext } from "../context/exhibit-context";
 
 const Exhibits = () => {
   const { exhibit } = useContext(ExhibitContext);
-  console.log(exhibit, "showing in the exhibit page");
+
 
   if (exhibit.length === 0) {
     return (
@@ -25,6 +25,8 @@ const Exhibits = () => {
               <article>
                 {artwork.images.length != 0 ? (
                   <img
+                  width={300}
+                  height="auto"
                     src={`https://framemark.vam.ac.uk/collections/${artwork.images[0]}/full/600,400/0/default.jpg`}
                     alt={
                       artwork.physicalDescription ? (
@@ -35,26 +37,39 @@ const Exhibits = () => {
                     }
                   ></img>
                 ) : (
-                  <p>placeholder insert here</p>
+                  <img 
+                  
+                  width={300}
+                  height="auto"
+                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/660px-No-Image-Placeholder.svg.png?20200912122019" />
                 )}
-                <p>© Victoria and Albert Museum, London</p>
+
+                <p className="subtitle subtitle-6">© Victoria and Albert Museum, London</p>
                 <p>ID: {artwork.systemNumber}</p>
                 <p>
                   Found in V&A Museum Collection, on shelf
-                  {artwork.galleryLocations[0].shelf}, box
-                  {artwork.galleryLocations[0].box}, in case
-                  {artwork.galleryLocations[0].case}
+                  {artwork.galleryLocations[0].shelf ? (
+                    <span> {artwork.galleryLocations[0].shelf}</span>
+                  ) : (
+                    <span>N/A</span>
+                  )}
+                  , box
+                  {artwork.galleryLocations[0].box ? (
+                    <span> {artwork.galleryLocations[0].box} </span>
+                  ) : (
+                    <span> N/A</span>
+                  )}
+                  , in case
+                  {artwork.galleryLocations[0].case ? (
+                    <span> {artwork.galleryLocations[0].case} </span>
+                  ) : (
+                    <span> N/A</span>
+                  )}
                 </p>
-                <p>
-                  Contact
-                  <a
-                    target="blank"
-                    href="https://www.vam.ac.uk/info/contact-us"
-                  >
-                    Victoria and Albert Museum
-                  </a>
-                  regarding their pieces
-                </p>
+
+                <a target="blank" href="https://www.vam.ac.uk/info/contact-us">
+                  Victoria and Albert Museum
+                </a>
               </article>
             );
           } else {
@@ -70,16 +85,13 @@ const Exhibits = () => {
                   Found in Cleveland Museum of Art, {artwork.department},
                   {artwork.currentLocation}
                 </p>
-                <p>
-                  Contact
-                  <a
-                    target="blank"
-                    href="https://www.clevelandart.org/contact-us"
-                  >
-                    Cleveland Museum of Art
-                  </a>
-                  regarding their pieces
-                </p>
+
+                <a
+                  target="blank"
+                  href="https://www.clevelandart.org/contact-us"
+                >
+                  Cleveland Museum of Art
+                </a>
               </article>
             );
           }
