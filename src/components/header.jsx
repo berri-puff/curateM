173d94a5cc779e1dd81442809beccea36935021a
berri-puff <jennyzhang0207@gmail.com>
2024-08-01 13:23:57 +0100

    styling the header with logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,11 @@
 import { Link} from "react-router-dom"
+import { GiWoodFrame } from "react-icons/gi";
+
 
 const Header = ()=>{
     return <Link to="/">
-        <h1 className="has-text-centered">Curate'M</h1>
+        <h1 id="main-title" className="has-text-centered"> <GiWoodFrame />
+        Curate'M</h1>
     </Link>
 }
 
