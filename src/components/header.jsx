d6c65b7ff381d5ad549852f665ebc3b4557a9748
berri-puff <jennyzhang0207@gmail.com>
2024-08-01 14:38:57 +0100

    rearrange nav bar and adds header shadow

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,19 @@
 import { Link} from "react-router-dom"
 import { GiWoodFrame } from "react-icons/gi";
+import Navigation from "./nav";
 
 
 const Header = ()=>{
-    return <Link to="/">
-        <h1 id="main-title" className="has-text-centered"> <GiWoodFrame />
+    return (
+        <section className="level">
+              <Link to="/">
+        <h1 id="main-title" className="has-text-centered level-left"> <GiWoodFrame />
         Curate'M</h1>
     </Link>
+    <Navigation className="level-right"/>
+        </section>
+      
+)
 }
 
 export default Header
